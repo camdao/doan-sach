d1deb0f97b8171ef9a0bfe6c691db586169035ee
xuanduy2302 <152255834+xuanduy2302@users.noreply.github.com>
2023-11-29 22:20:28 +0700

    Update QuanLyDSNhanVien.java

--- DoAn/src/NHANVIEN/QuanLyDSNhanVien.java
+++ DoAn/src/NHANVIEN/QuanLyDSNhanVien.java
@@ -1,24 +1,22 @@
-package NHANVIEN;
-
+package NhanVien;
 import java.util.Scanner;
 
 
-public class QuanLyDSNhanVien {
+class QuanLyDSNhanVien {
     public DSNhanVien dsNhanVien;
-            Scanner sc = new Scanner(System.in);
-
     public QuanLyDSNhanVien(){
     	dsNhanVien = new DSNhanVien();
     }
-    public void Menu(){
+    void Menu(){
         int lua_chon;
+       Scanner sc=new Scanner(System.in);
        	boolean flag=true;
         while(flag){
-            System.out.println(" 1/Them nhan vien.\n 2/Xoa nhan vien.\n 3/Sua nhan vien.\n 4/Xuat danh sach nhan vien.\n 5/Tim kiem nhan vien.\n 6.Quay lai.\n 7.Thoat\n");
+            System.out.println(" 1/Them nhan vien.\n 2/Xoa nhan vien.\n 3/Sua nhan vien.\n 4/Xuat danh sach nhan vien.\n 5/Tim kiem nhan vien.\n 6.Thoat.");
             lua_chon=sc.nextInt();
             switch(lua_chon){
                 case 1:
-					dsNhanVien.them();
+		    dsNhanVien.them();
                     break;
                 case 2:
                     dsNhanVien.xoa();
@@ -34,13 +32,10 @@ public class QuanLyDSNhanVien {
                     break;
                 case 6:
                     flag=false;
-                    DSNhanVien.WriteFile();
                     break;
-                case 7:
-                    DSNhanVien.WriteFile();
-                    System.exit(0);
            }
         }
     }
 
 }
+ 
